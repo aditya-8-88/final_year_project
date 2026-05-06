da9f8d6d5b86f3c0bba4981e2004a90aa3970c1b
aditya-8-88 <adityapratapsingh12b05@gmail.com>
2026-05-06 21:43:52 +0530

    fix: use CardThemeData for Flutter 3.41.9 compatibility; add SOS emergency feature

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -34,7 +34,7 @@ class AppTheme {
             color: Colors.white,
           ),
         ),
-        cardTheme: CardTheme(
+        cardTheme: CardThemeData(
           elevation: 2,
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
@@ -127,7 +127,7 @@ class AppTheme {
             color: Colors.white,
           ),
         ),
-        cardTheme: CardTheme(
+        cardTheme: CardThemeData(
           elevation: 2,
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
